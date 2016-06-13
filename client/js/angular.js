909ec1136b781b158c9e84068fcb135cce9cb5ce
Tosh Camille <tochecamille@gmail.com>
2016-06-13 18:09:27 +0200

    refresh every second

--- client/js/angular.js
+++ client/js/angular.js
@@ -1,6 +1,12 @@
 var app = angular.module('gpu', []);
+
 app.controller('ctrl', function($scope, $http) {
-	$http.get("http://ipServer:6969/") .then(function (response) {
-		$scope.names = response.data.data;
-	});
+	var gpu = function gpuInfos() {
+	$http.get("http://ipServer:6969") .then(function (response) {
+		$scope.infos = response.data.data;
+		console.log("toast");
+	});}
+	setInterval(gpu, 1000);
 });
+
+
