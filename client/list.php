e86c371c93ff8616b2ba5e1d450c774e3eb44928
Tosh Camille <tochecamille@gmail.com>
2016-08-06 17:30:51 +0200

    fix

--- client/list.php
+++ client/list.php
@@ -11,7 +11,47 @@
 			$file = file_get_contents("config.json");
 			$f = json_decode($file);
 
-			$miner = $f->worker[0];
+			$miner = $f->master;
+			$i = 0;
+
+			foreach ($miner as $m) {
+				?>
+		<thead>
+			<tr>
+				<th>Master <?= $i ?></th>
+				<th>Link</th>
+				<th>Status</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+				$r = 0;
+				foreach ((array) $m as $k) {
+
+				?>
+		<tr>
+			<td><a href="http://<?= $k ?>" target="_blank"><?= $k ?></a></td>
+			<td>
+			<a href="single.php?group=<?= $i ?>&rig=<?= $r ?>" target="_blank">Single page</a>
+			</td>
+			<td>At work</td>
+		</tr>
+		<?php
+				$r++;
+			}
+			$i++;
+				?>
+		</tbody>
+	<?php
+		}
+		?>
+	</table>	<table>
+	<?php
+			
+			$file = file_get_contents("config.json");
+			$f = json_decode($file);
+
+			$miner = $f->worker;
 			$i = 0;
 
 			foreach ($miner as $m) {
@@ -26,7 +66,7 @@
 		<tbody>
 		<?php
 				$r = 0;
-				foreach ($m as $k) {
+				foreach ((array) $m as $k) {
 
 				?>
 		<tr>
